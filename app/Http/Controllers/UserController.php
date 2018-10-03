85ce3eee94b43b81ef54633d6db5ef2e59e62499
unknown <jnogra@stackify.com>
2018-10-03 17:03:12 +0800

    Random string for the login token.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,18 @@ class UserController extends Controller
     	$new_user->gender = $request->gender;
     	$new_user->education = $request->education;
     	$new_user->password = Hash::make($request->password);
+    	$new_user->login_token = generateRandomString(16);
     	$new_user->save();
     	return $new_user;
     }
+
+	function generateRandomString($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
 }
